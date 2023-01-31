ddcde92c107184594b39f3ab782361e3a844400b
Madhurya-15 <madhurya.vankayala@gmail.com>
2023-01-30 16:25:57 -0800

    Changed the name

--- Demo_Synchronization_Method/src/com/company/Main.java
+++ Demo_Synchronization_Method/src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Thread1 t1 = new Thread1(test1);
         Thread2 t2 = new Thread2(test1);
 
-        t1.setName("Infosys_1");
+        t1.setName("Infosys_12");
         t2.setName("Infosys_2");
         t1.start();
         t2.start();
